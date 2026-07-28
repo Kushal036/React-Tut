0011a1c95df3d870b2afc8a801c66dc6023e9927
Kushal <mittan.kushal1727@gmail.com>
2026-07-28 15:58:14 +0530

    again project 12

---
+++ 12_todo_context_local01/src/context/TodoContext.js
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+    todos :[
+        {
+            id:1,
+            todo : "todo message",
+            completed :false
+        }
+    ],
+    addTodo : (todo)=>{},
+    updateTodo : (id,todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleTodo : (id)=>{}
+
+})
+
+export const TodoProvider = TodoContext.Provider 
